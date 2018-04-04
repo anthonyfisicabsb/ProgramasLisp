adb02fb54f1f482733af7ae6901acf1052cd6ab9
Anthony Louis <anthony.physis@gmail.com>
2018-04-04 04:07:25 -0300

    operacoes basicas lisp

---
+++ basicaritmetic.lisp
@@ -0,0 +1,8 @@
+(print (+ 8 9) );; soma de números
+(print (- 4 3)) ;; subtração de números
+(print (* 5 3) );; multiplicação de números
+(print (/ 10 2)) ;; divisão de números
+(print (+ #C(1 2) #C(-5 4))) ;; adição de números complexos
+(print (expt 3 5) );; exponencial de números
+(print (mod 11 3)) ;; módulo entre os números
+(print (* 3.870 4.89)) ;; operações entre pontos flutuantes
